a9a2d579ecaa20074b1964f08b5705ed4d352848
Matheus <matheus.victor.ss48@gmail.com>
2026-03-26 10:20:25 -0300

    coluna funcionando com dados inseridos, datya inserida e exclusão dos itens

--- script.js
+++ script.js
@@ -13,3 +13,44 @@ function selecionar(motivo){
     motivoSelecionado=motivo
     document.getElementById('escolhido').innerText = "selecionado: " + motivo
 }
+//verifica se ta completa
+btn.addEventListener('click', ()=>{
+    let descricao = document.getElementById('text').value
+    let valor = document.getElementById('num').value
+
+    if(descricao == "" || valor == "" || motivoSelecionado == ""){
+        alert("Preencha tudo!")
+        return
+    }
+
+    let tabela = document.getElementById("tabela").getElementsByTagName('tbody')[0]
+
+    let novaLinha = tabela.insertRow()//adicionar linha nova na tabela
+//indice de cada item
+    novaLinha.insertCell(0).innerText = descricao
+    novaLinha.insertCell(1).innerText = valor
+    novaLinha.insertCell(2).innerText = motivoSelecionado
+    novaLinha.insertCell(3).innerText = "Gasto"
+    //pega a data do momento
+    let data = new Date().toLocaleDateString()
+    novaLinha.insertCell(4).innerText = data
+
+    // botão excluir
+    let celulaAcao = novaLinha.insertCell(5)
+    let btnExcluir = document.createElement("button")
+    btnExcluir.innerText = "Excluir"
+
+    btnExcluir.onclick = function(){
+        novaLinha.remove()
+    }
+
+    celulaAcao.appendChild(btnExcluir)
+
+    // limpar campos
+    document.getElementById('text').value = ""
+    document.getElementById('num').value = ""
+    document.getElementById('escolhido').innerText = "Nenhum selecionado"
+    motivoSelecionado = ""
+
+    cont.close()
+})
