557977c8987eca7fe8767cc49ca6b848832fd837
Matheus <matheus.victor.ss48@gmail.com>
2026-03-27 09:37:07 -0300

    cadastro agora esta funcionando corretamente

--- salvar.php
+++ salvar.php
@@ -1,21 +1,22 @@
 <?php
-$host = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "controle_financeiro";
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    die("Acesso inválido!");
+}
 
 $conn = new mysqli("localhost", "root", "", "controle_financeiro");
-//testando se ta conectando
+
 if ($conn->connect_error) {
     die("Erro de conexão: " . $conn->connect_error);
 }
-//pegando os valores
+
+
+var_dump($_POST);
+
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
 $salario = $_POST['salario'];
 
-// inserir no banco
 $sql = "INSERT INTO usuarios (nome, email, senha, salario)
         VALUES ('$nome', '$email', '$senha', '$salario')";
 
